875e3a9d606aa446f1b59d879eb0e947bbedb4d6
doraxxx-commits <dorax.xx@o2.pl>
2026-08-22 21:30:10 +0200

    Update game_engine.dart

--- lib/core/game_engine.dart
+++ lib/core/game_engine.dart
@@ -1,11 +1,30 @@
+import '../data/world_data.dart';
+import '../models/club.dart';
+import '../models/league.dart';
+import '../models/player.dart';
+import '../simulation/league_engine.dart';
 import 'game_state.dart';
 
 class GameEngine {
   final GameState state;
 
+  late final List<League> leagues;
+  late final List<Club> clubs;
+  late final List<Player> players;
+
+  late final LeagueEngine leagueEngine;
+
   GameEngine({
     GameState? state,
-  }) : state = state ?? GameState();
+  }) : state = state ?? GameState() {
+    leagues = WorldData.leagues;
+    clubs = WorldData.clubs;
+    players = WorldData.players;
+
+    leagueEngine = LeagueEngine(
+      clubs: clubs,
+    );
+  }
 
   void advanceDay() {
     state.nextDay();
@@ -25,5 +44,11 @@ class GameEngine {
 
   bool get winterTransferWindow {
     return state.transferWindowWinter;
+  }
+
+  List<Club> get leagueClubs {
+    return clubs.where((club) {
+      return club.leagueId == 'pol_ek';
+    }).toList();
   }
 }
