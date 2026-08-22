44cf9865d3061ff4ffab15a2ea0f183015d20f00
doraxxx-commits <dorax.xx@o2.pl>
2026-08-22 22:55:19 +0200

    Update player_contract.dart

--- lib/models/player_contract.dart
+++ lib/models/player_contract.dart
@@ -23,3 +23,29 @@ class PlayerContract {
     this.managerTrust = 50,
   });
 }
+
+import 'squad_status.dart';
+
+class PlayerContract {
+  String clubId;
+
+  int yearsRemaining;
+  double weeklySalary;
+  double marketValue;
+
+  int squadNumber;
+
+  SquadStatus squadStatus;
+
+  int managerTrust;
+
+  PlayerContract({
+    required this.clubId,
+    required this.yearsRemaining,
+    required this.weeklySalary,
+    required this.marketValue,
+    required this.squadNumber,
+    this.squadStatus = SquadStatus.reserves,
+    this.managerTrust = 50,
+  });
+}
