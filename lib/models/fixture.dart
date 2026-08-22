818911218dafbd3f5ab67a290b63270fbf9b9371
doraxxx-commits <dorax.xx@o2.pl>
2026-08-22 21:46:15 +0200

    Update fixture.dart

--- lib/models/fixture.dart
+++ lib/models/fixture.dart
@@ -3,6 +3,10 @@ class Fixture {
   final String homeClubId;
   final String awayClubId;
 
+  final int year;
+  final int month;
+  final int day;
+
   bool played;
   int? homeGoals;
   int? awayGoals;
@@ -11,6 +15,9 @@ class Fixture {
     required this.round,
     required this.homeClubId,
     required this.awayClubId,
+    required this.year,
+    required this.month,
+    required this.day,
     this.played = false,
     this.homeGoals,
     this.awayGoals,
