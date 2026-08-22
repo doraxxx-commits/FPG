324c4c426f4230113fb6e6ff8bd356a5d9fa0c40
doraxxx-commits <dorax.xx@o2.pl>
2026-08-22 21:36:22 +0200

    Create match_result.dart

---
+++ lib/models/match_result.dart
@@ -0,0 +1,14 @@
+class MatchResult {
+  final String homeClubId;
+  final String awayClubId;
+
+  final int homeGoals;
+  final int awayGoals;
+
+  MatchResult({
+    required this.homeClubId,
+    required this.awayClubId,
+    required this.homeGoals,
+    required this.awayGoals,
+  });
+}
