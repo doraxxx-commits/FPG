d82e06daec8fa3d4e3eef6d18f4e5ec332ca8126
doraxxx-commits <dorax.xx@o2.pl>
2026-08-22 22:07:18 +0200

    Create player_contract.dart

---
+++ lib/models/player_contract.dart
@@ -0,0 +1,25 @@
+class PlayerContract {
+  String clubId;
+
+  int yearsRemaining;
+
+  double weeklySalary;
+
+  double marketValue;
+
+  int squadNumber;
+
+  String squadStatus;
+
+  int managerTrust;
+
+  PlayerContract({
+    required this.clubId,
+    required this.yearsRemaining,
+    required this.weeklySalary,
+    required this.marketValue,
+    required this.squadNumber,
+    this.squadStatus = 'Młody zawodnik',
+    this.managerTrust = 50,
+  });
+}
